ec97e33200bf4183608f3cd45fb40a5ef8d3ee50
yoloroy <slart_ru@mail.ru>
2020-07-13 10:39:52 +0300

    :bug: solve rerender problem

--- app/src/main/java/yoloyoj/pub/ui/chat/MessageItemHolder.kt
+++ app/src/main/java/yoloyoj/pub/ui/chat/MessageItemHolder.kt
@@ -4,19 +4,22 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_message.view.*
 import yoloyoj.pub.models.Message
+import yoloyoj.pub.utils.toDp
 
 class MessageItemHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(message: Message, showUserName: Boolean, showUserAvatar: Boolean) {
         view.textView.text = message.text
 
-        if (showUserName)
+        if (showUserName) {
+            view.senderView.visibility = View.VISIBLE
             view.senderView.text = message.sender
+        }
         else
             view.senderView.visibility = View.GONE
 
-        if (showUserAvatar)
-            // for future avatar viewing
-        else {
+        if (showUserAvatar) {
+            view.showAvatar()
+        } else {
             view.hideAvatar()
         }
     }
@@ -25,14 +28,13 @@ class MessageItemHolder(private val view: View) : RecyclerView.ViewHolder(view) 
         userView.visibility = View.INVISIBLE
         userView.layoutParams.height = 0
 
-        avatarArrow.visibility = View.INVISIBLE
-        messageBox.apply {
-            setPadding(
-                paddingLeft,
-                paddingTop,
-                paddingRight,
-                0
-            )
-        }
+        avatarArrow.visibility = View.GONE
+    }
+
+    private fun View.showAvatar() {
+        userView.visibility = View.VISIBLE
+        userView.layoutParams.height = 48.toDp(userView)
+
+        avatarArrow.visibility = View.VISIBLE
     }
 }
---
+++ app/src/main/java/yoloyoj/pub/utils/other.kt
@@ -0,0 +1,11 @@
+package yoloyoj.pub.utils
+
+import android.util.TypedValue
+import android.view.View
+
+fun Number.toDp(view: View) =
+    TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        view.resources.displayMetrics
+    ).toInt()
