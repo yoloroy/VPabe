db7ba59e9ce9cac0d933b93678efad102fc01cb9
yoloroy <slart_ru@mail.ru>
2020-08-02 16:14:39 +0300

    :recycle: delete extra fun

--- app/src/main/java/yoloyoj/pub/web/handlers/SingleEventGetter.kt
+++ app/src/main/java/yoloyoj/pub/web/handlers/SingleEventGetter.kt
@@ -4,17 +4,10 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import yoloyoj.pub.models.Event
-import yoloyoj.pub.web.apiClient
 
 class SingleEventGetter(
     var eventHandler: ((Event?) -> Unit)
 ) : Callback<Event?> {
-
-    fun start(
-        eventId: Int = 0
-    ) {
-        apiClient.getSingleEvent(eventId)?.enqueue(this)
-    }
 
     override fun onFailure(call: Call<Event?>, t: Throwable) {
         eventHandler(null)
